d55145183bfa27bab1bb7181db3c87d53fa19474
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-17 20:12:12 +0300

    Update run.py

--- run.py
+++ run.py
@@ -60,9 +60,9 @@ class Metric: # metric class for storing metrics (accuracy, loss)
 
 epoch = 3
 print_freq = 500
-batch_size = 6
+batch_size = 4
 max_len = 256
-accumulation_steps = 12
+accumulation_steps = 16
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 data_dir=''
