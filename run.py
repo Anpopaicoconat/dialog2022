dca61a6a199a2fc199f0459ca07584977aacb749
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-20 06:10:41 +0300

    Update run.py

--- run.py
+++ run.py
@@ -65,7 +65,7 @@ max_len = 256
 accumulation_steps = 128
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-data_dir=''
+data_dir='multi'
 train = pd.read_csv(data_dir + 'train.csv')
 test = pd.read_csv(data_dir + 'test.csv')
 val = pd.read_csv(data_dir + 'val.csv')
@@ -93,8 +93,8 @@ for i, m in enumerate(model.transformer.h):
         for parameter in m.parameters():
             parameter.requires_grad = True 
 
-for parameter in model.transformer.ln_f.parameters():        
-    parameter.requires_grad = True
+#for parameter in model.transformer.ln_f.parameters():        
+#    parameter.requires_grad = True
 
 optimizer = transformers.AdamW(filter(lambda p: p.requires_grad, model.parameters()),
                                lr = lr, # default is 5e-5, our notebook had 2e-5
@@ -115,36 +115,35 @@ print('\nload ru_gpt_bi-classifier')
 model.load_state_dict(torch.load('ru_gpt_bi-classifier.pt')) 
 
 for i_epoch in range(epoch):
-#    model.train()
-#    i_batch = 0
-#    losses = 0
-#    accs = 0
-#    ns = 0
-#    for batch in tqdm(train_loader):
-#        i_batch+=1
-#        batch = {k:batch[k].to(model.device) for k in batch}
-#        labels = batch.pop('Class')
-#        out = model(**batch, labels=labels)
-#        logits = out.logits
-#        pred = logits.argmax(axis=1).to('cpu').detach()
-#        accs += sum(pred == labels.to('cpu').detach()).double()
-#        ns += len(pred)
+    model.train()
+    i_batch = 0
+    losses = 0
+    accs = 0
+    ns = 0
+    for batch in tqdm(train_loader):
+        i_batch+=1
+        batch = {k:batch[k].to(model.device) for k in batch}
+        labels = batch.pop('Class')
+        out = model(**batch, labels=labels)
+        logits = out.logits
+        pred = logits.argmax(axis=1).to('cpu').detach()
+        accs += sum(pred == labels.to('cpu').detach()).double()
+        ns += len(pred)
 
-#        loss = out.loss
-#        losses += loss.to('cpu').detach()
-#        (loss / accumulation_steps).backward()
+        loss = out.loss
+        losses += loss.to('cpu').detach()
+        (loss / accumulation_steps).backward()
 #        
-#        if (i_batch % accumulation_steps == 0) or (i_batch == len(train_loader)):
-#            optimizer.step()
-#            optimizer.zero_grad()
-#            
-#        if i_batch % (print_freq * accumulation_steps) == 0:
-#            print('loss:', losses/ns, 'acc:', accs/ns)
-
+        if (i_batch % accumulation_steps == 0) or (i_batch == len(train_loader)):
+            optimizer.step()
+            optimizer.zero_grad()
+            
+        if i_batch % (print_freq * accumulation_steps) == 0:
+            print('loss:', losses/ns, 'acc:', accs/ns)
         
-#    print('\n\nepoch', i_epoch, '\nloss:', losses/ns, 'acc:', accs/ns, '\n\n')
-#    torch.save(model.state_dict(), "ru_gpt_bi-classifier.pt")
-#    torch.cuda.empty_cache()
+    print('\n\nepoch', i_epoch, '\nloss:', losses/ns, 'acc:', accs/ns, '\n\n')
+    torch.save(model.state_dict(), "ru_gpt_bi-classifier.pt")
+    torch.cuda.empty_cache()
     
     #val
     model.eval()
@@ -168,7 +167,6 @@ for i_epoch in range(epoch):
         
         if val_i_batch % (print_freq * accumulation_steps) == 0:
             print('val_acc:', val_accs/val_ns) #'val_loss:', val_losses/val_ns, 
-    print('val_acc:', val_accs/val_ns)
     print('='*10, '\n\nepoch', i_epoch, '\nloss:', losses/ns, 'acc:', accs/ns, 'val_acc:', val_accs/val_ns, '\n\n', '='*10) #'val_loss:', val_losses/val_ns, 
     
     
