48e0aef06862f6dab176b84498da741db7cd461f
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-21 13:26:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -85,7 +85,7 @@ models_dir = '/home/posokhov@ad.speechpro.com/projects/models/'
 model_name = "ruRoberta-large"
 model_path = models_dir+model_name
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
-model = model = transformers.RobertaForSequenceClassification().from_pretrained(model_path, num_labels=n_classes, local_files_only=True)
+model = model = transformers.RobertaForSequenceClassification.from_pretrained(model_path, num_labels=n_classes, local_files_only=True)
 
 model.to(device)
 
